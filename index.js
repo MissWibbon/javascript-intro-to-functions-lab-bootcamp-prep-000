ce2f37c6a921a7c864e7d005c0c26e2d749dee34
Michelle Wuebben <wuebbenmichelle@gmail.com>
2019-05-01 19:40:46 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string){
+  return string.toUpperCase()
+}
+shout('hello');
+
+function logShout(string){
+  return console.log(string.toUpperCase())
+}
+logShout('hello')
+
+function whisper(string){
+  return string.toLowerCase()
+}
+whisper('HELLO')
+function logWhisper(string){
+  return string.toLowerCase()
+}
+logWhisper('HELLO')
+function sayHiToGrandma(string){
+  if(uppercase.toUpperCase() === uppercase) {
+  return 'YES INDEED!'
+  }
+  else if(lowercase.toLowerCase() === lowercase){
+  return 'I can\'t hear you!'
+    
+  }
+}
